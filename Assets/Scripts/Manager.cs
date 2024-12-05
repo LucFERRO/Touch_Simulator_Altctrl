fb280ac8f1679862dc5614369f9f92afade2e72f
Luc FERRO <luchenri.ferro@gmail.com>
2024-12-05 12:13:56 +0100

    attack, shield, jump and dual projectiles done. Needs assets and menu

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class Manager : MonoBehaviour
 {
+    [Header("Difficulty Settings")]
+    public float timePassed;
     public float difficultyScaling;
     public float startingDifficulty;
+    public float chancesOfDoubleProjectiles;
+    public float chancesOfDoubleSaws;
+    [Header("Spawners Position")]
+    public bool areSpawnerVisible;
+    public float midRowHeight;
+    public float sideDistance;
     public Transform playerTransform;
     public Transform spawnersPos;
     public GameObject topSpawner;
@@ -18,11 +26,6 @@ public class Manager : MonoBehaviour
     public GameObject projectile1;
     public GameObject topProjectile;
     public GameObject[] smallProjectileSpawners;
-    public float timePassed;
-    [Header("Spawners Position")]
-    public bool areSpawnerVisible;
-    public float midRowHeight;
-    public float sideDistance;
 
     void Start()
     {
@@ -63,26 +66,67 @@ public class Manager : MonoBehaviour
         {
             GameObject createdProjectile = null;
             int randomSpawnerInt = Random.Range(0, 6);
-            if (randomSpawnerInt < 3)
+            if (randomSpawnerInt == 0)
             {
-                createdProjectile = SpawnSmallProjectile(randomSpawnerInt);
+                createdProjectile = SpawnSmallProjectile(1);
             }
-            else if (randomSpawnerInt % 2 == 1)
+            else if (randomSpawnerInt < 3)
             {
-                createdProjectile = SpawnSaw(randomSpawnerInt == 3);
+                bool leftOrRightProjectile = randomSpawnerInt == 1;
+                ExtraSpawn(projectile1, chancesOfDoubleProjectiles, leftOrRightProjectile);
+                createdProjectile = SpawnSmallProjectile(leftOrRightProjectile ? 2 : 0);
+            }
+            else if (randomSpawnerInt < 5)
+            {
+                bool leftOrRightSaw = randomSpawnerInt == 3;
+                ExtraSpawn(saw, chancesOfDoubleSaws, !leftOrRightSaw);
+                createdProjectile = SpawnSaw(leftOrRightSaw);
             }
             else
             {
                 createdProjectile = SpawnTopProjectile();
             }
+
             createdProjectile.GetComponent<Projectile>().playerTransform = playerTransform;
             createdProjectile.GetComponent<Projectile>().speed = startingDifficulty + difficultyScaling;
+
+            IncreaseDifficulty();
+            timePassed = 0f;
+
+        }
+
+        void IncreaseDifficulty()
+        {
             if (difficultyScaling < 40)
             {
                 difficultyScaling += 2f;
             }
-            timePassed = 0f;
+        }
 
+        void ExtraSpawn(GameObject item, float chancesOfDouble, bool isLeftOrRight)
+        {
+            if (difficultyScaling < 20){
+                return;
+            }
+
+            bool doubledProj = Random.Range(0, 100) < chancesOfDouble;
+            if (!doubledProj)
+            {
+                return;
+            }
+
+            GameObject extraProjectile;
+
+            if (item == saw)
+            {
+                extraProjectile = SpawnSaw(isLeftOrRight);
+            } else
+            {
+                extraProjectile = SpawnSmallProjectile(isLeftOrRight ? 0 : 2);
+            }
+
+            extraProjectile.GetComponent<Projectile>().playerTransform = playerTransform;
+            extraProjectile.GetComponent<Projectile>().speed = startingDifficulty + difficultyScaling;
         }
 
         GameObject SpawnTopProjectile()
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,16 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
+public static class Utility
+{
+    public static void Invoke(this MonoBehaviour mb, Action f, float delay)
+    {
+        mb.StartCoroutine(InvokeRoutine(f, delay));
+    }
+
+    private static IEnumerator InvokeRoutine(System.Action f, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        f();
+    }
+}
 public class PlayerControl : MonoBehaviour
 {
     public int hp;
     public int maxHp;
     public int tiltAngle;
     public int crouchAngle;
+    public float attackDuration;
     public GameObject leftAttack;
     public GameObject rightAttack;
+    public GameObject shield;
     private Vector3 startingPos;
     void Start()
     {
@@ -20,20 +36,40 @@ public class PlayerControl : MonoBehaviour
 
     void Update()
     {
-        SideDodgeHandler();
+        SawDodgeHandler();
         CrouchHandler();
-        AttackHandler(leftAttack, Input.GetKey(KeyCode.R));
-        AttackHandler(rightAttack, Input.GetKey(KeyCode.T));
+        AttackHandler();
+        ShieldHandler();
         if (hp <= 0)
         {
             Debug.Log("YOU DIED");
         }
     }
 
-    void AttackHandler(GameObject attackSide, bool attackCommand)
+    void AttackHandler()
     {
-        attackSide.SetActive(attackCommand);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            leftAttack.SetActive(true);
+            this.Invoke(() => leftAttack.SetActive(false), attackDuration);            
+        }        
+
+        if (Input.GetKeyDown(KeyCode.Y))
+        {
+            rightAttack.SetActive(true);
+            this.Invoke(() => rightAttack.SetActive(false), attackDuration);
+        }
     }
+
+    void ShieldHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            shield.SetActive(true);
+            this.Invoke(() => shield.SetActive(false), attackDuration);
+        }
+    }
+
     void CrouchHandler()
     {
         if (Input.GetKey(KeyCode.E))
@@ -50,7 +86,7 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
-    void SideDodgeHandler()
+    void SawDodgeHandler()
     {
         if (Input.GetKey(KeyCode.Q) && Input.GetKey(KeyCode.W))
         {
@@ -72,6 +108,12 @@ public class PlayerControl : MonoBehaviour
             Quaternion newRotation = Quaternion.AngleAxis(tiltAngle, Vector3.forward);
             transform.position = Vector3.Lerp(transform.position, newPos, .05f);
             transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, .05f);
+        }
+        if (!Input.GetKey(KeyCode.Q) && !Input.GetKey(KeyCode.W))
+        {
+            Vector3 newPos = transform.position;
+            newPos.y = startingPos.y + 1;
+            transform.position = Vector3.Lerp(transform.position, newPos, .05f);
         }
     }
 
